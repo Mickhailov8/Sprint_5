470d311d071fd380d8fc289e6fe809134afa1268
Michailov <Mexxes888@yandex.ru>
2024-08-22 11:49:42 +0300

    first

--- Constants.py
+++
@@ -1,7 +0,0 @@
-class Constants:
-    Website_Url = 'https://stellarburgers.nomoreparties.site/'  # Адрес сайта
-
-    Test_Name = 'Кирилл'  # Тестовое имя
-    Test_Email = 'Kirill_Mickhailov_12_136@yandex.ru'  # Тестовый mail
-    Test_Password = '123456'  # Тестовый пароль
-    Wrong_Password = '12345'  # Неверный пароль
